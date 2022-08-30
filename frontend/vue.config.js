e985c180fff64db20afe99970d35185c32fd3c10
SergioMartinHernandez <99206081+SergioMartinHernandez@users.noreply.github.com>
2022-08-30 18:10:10 +0200

    Delete frontend directory

--- frontend/vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave:false,
-  // publicPath: process.env.VUE_APP_SERVICE_URL,
-})
