9a56b0f2188494bb0cf5d38c07b638abcc54f6e9
SergioMartinHernandez <99206081+SergioMartinHernandez@users.noreply.github.com>
2022-09-02 19:25:56 +0200

    Add files via upload

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <section class="home_banner_area">
+    <div class="banner_inner">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            <div class="banner_content">
+              <img class="img-fluid" src="../assets/logo2.png" alt="">
+              <h4 class="text">Tool For Twitter Data Analysis</h4>
+              <div class="d-flex align-items-center">
+                <button id="login-button" type="button" class="btn btn-secondary btn-lg btn-block" @click="$router.push('/login')">Log In</button>
+                <button id="signup-button" type="button" class="btn btn-lg btn-dark btn-block" @click="$router.push('/signup')">Sign Up</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="home_right_img">
+              <img id="image-home" class="" src="../assets/home-image.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style scoped>
+#login-button {
+  margin-top: 20px; 
+  margin-right: 20px;
+  color: #fff;
+  background-color: #343a40;
+  border-color: #343a40;
+}
+#signup-button {
+  margin-top: 20px; 
+  color: #fff;
+  background-color: gray;
+  border-color: gray;
+}
+#image-home {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  margin-left: 20px;
+} 
+.home_banner_area {
+    z-index: 1;
+    background: url(../assets/home-banner.png) no-repeat top center;
+    background-position: center;
+    background-size: cover; }
+    .home_banner_area .banner_inner {
+      width: 100%; }
+      .home_banner_area .banner_inner .home_right_img {
+        padding-top: 197px; }
+        @media (max-width: 1480px) {
+          .home_banner_area .banner_inner .home_right_img img {
+            max-width: 100%;
+            height: auto; } }
+        @media (max-width: 991px) {
+          .home_banner_area .banner_inner .home_right_img {
+            display: none; } }
+      .home_banner_area .banner_inner .col-lg-7 {
+        vertical-align: middle;
+        align-self: center; }
+      .home_banner_area .banner_inner .banner_content {
+        text-align: left; }
+        @media (max-width: 991px) {
+          .home_banner_area .banner_inner .banner_content {
+            margin-top: 150px; } }
+        .home_banner_area .banner_inner .banner_content h3 {
+          font-size: 40px;
+          margin-bottom: 20px;
+          position: relative; }
+          .home_banner_area .banner_inner .banner_content h3:after {
+            content: '';
+            width: 410px;
+            height: 2px;
+            position: absolute;
+            top: 50%;
+            left: 23%;
+            background: #000000; }
+            @media (max-width: 575px) {
+              .home_banner_area .banner_inner .banner_content h3:after {
+                display: none; } }
+        .home_banner_area .banner_inner .banner_content h1 {
+          margin-top: 20px;
+          font-size: 70px;
+          line-height: 60px;
+          margin-bottom: 25px; }
+          @media (max-width: 1024px) {
+            .home_banner_area .banner_inner .banner_content h1 {
+              font-size: 60px; } }
+          @media (max-width: 767px) {
+            .home_banner_area .banner_inner .banner_content h1 {
+              font-size: 50px; } }
+        .home_banner_area .banner_inner .banner_content h5 {
+          font-size: 24px;
+          margin-bottom: 35px; }
+        .home_banner_area .banner_inner .banner_content .primary_btn {
+          margin-right: 20px; }
+</style>
---
+++ frontend/src/views/SearchHistory.vue
@@ -0,0 +1,86 @@
+<template>
+  <section class="section about-section gray-bg">
+    <div class="container">
+      <table class="table">
+        <thead>
+          <!-- Columnas tabla de historial de búsquedas  -->
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Type</th>
+            <th scope="col">Search Day</th>
+            <th scope="col">Start Date</th>
+            <th scope="col">End Date</th>
+            <th scope="col">Num Tweets</th>
+            <th scope="col">Repeat Search</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- Contenido tabla de historial de búsquedas -->
+          <tr v-for="(search, index) in user.searchs" :key="search.id">
+            <th scope="row">{{ index }}</th>
+            <td>{{ search.title }}</td>
+            <td>{{ search.type }}</td>
+            <td>{{ search.created_at }}</td>
+            <td>{{ search.start_date }}</td>
+            <td>{{ search.end_date }}</td>
+            <td>{{ search.num_tweets }}</td>
+            <td><button type="button" class="btn btn-primary" @click="RepeatSearch(search)">Repeat Search</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'SearchHistory',
+  created: function() {
+    this.$store.dispatch('viewMe');
+  },
+  computed: {
+    ...mapGetters({user: 'stateUser' }),
+  },
+  methods: {
+    ...mapActions(['createSearch']),
+    async RepeatSearch(search) {
+      // Creacion de busqueda de tweets
+      if(search.type=="Tweet") {
+          try {
+              await this.createSearch(search);
+              await this.$store.dispatch('viewTweetSearch');
+              this.$router.push('/tweetanalysis');
+          } catch (error) {
+              throw 'Error in create search tweet. Please try again.';
+          }
+      }
+      // Creacion de busqueda de usuarios
+      else if(search.type=="User"){
+          try {
+              await this.createSearch(search);
+              await this.$store.dispatch('viewUserSearch');
+              await this.$store.dispatch('viewTweetSearch');
+              this.$router.push('/useranalysis');
+          } catch (error) {
+              throw 'Error in create search user. Please try again.';
+          }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.section {
+    padding: 100px 0;
+    position: relative;
+}
+.gray-bg {
+    background-color: #f5f5ff;
+    height: 100%;
+}
+</style>
